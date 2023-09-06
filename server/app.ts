3b4d100b4b5a77a65370b8e863324645e7997675
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-06 21:46:40 +0600

    body parser middleware added

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,7 @@
 import express from "express";
-
 export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//body parse
+app.use(express.json({ limit: "50mb" }));
