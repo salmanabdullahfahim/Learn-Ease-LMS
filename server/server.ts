29f8a4b1d75d783f2797dd22cddfe8631d9fb61c
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-07 12:15:23 +0600

    database connected with mongodb through mongoose

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,9 @@
 import { app } from "./app";
+import dbConnect from "./utils/db";
 require("dotenv").config();
 
 //listen server
 app.listen(process.env.PORT, () => {
   console.log(`server listening on port ${process.env.PORT}`);
+  dbConnect();
 });
