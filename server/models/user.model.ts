07f2cd801e9a65a44ba8f0ecc3e5cb4fb0a92624
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-08 09:46:33 +0600

    user model created

---
+++ server/models/user.model.ts
@@ -0,0 +1,18 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+
+const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  isVerified: boolean;
+  courses: Array<{ courseId: string }>;
+  comparePassword: (password: string) => Promise<boolean>;
+}
