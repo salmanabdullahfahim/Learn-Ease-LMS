fb107d91eec71c02adcf4392adba9f57c2ad5271
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-08 21:10:33 +0600

    import all important things in user controller

---
+++ server/controllers/user.controller.ts
@@ -0,0 +1,4 @@
+import { Request, Response, NextFunction } from "express";
+import userModel, { IUser } from "../models/user.model";
+import ErrorHandler from "../utils/ErrorHandler";
+import { catchAsyncError } from "../middleware/catchAsyncErrors";
