a8896d775655a6b773be7a8eedac69af95ccc19c
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-08 21:19:18 +0600

    register body interface created

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -2,3 +2,11 @@ import { Request, Response, NextFunction } from "express";
 import userModel, { IUser } from "../models/user.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import { catchAsyncError } from "../middleware/catchAsyncErrors";
+
+//register user
+interface IRegisterBody {
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string;
+}
