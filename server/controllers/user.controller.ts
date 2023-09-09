9eba904ed2295011926dfe84c183b74a05ba7140
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-09 19:18:46 +0600

    registrationUser function created

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -10,3 +10,24 @@ interface IRegisterBody {
   password: string;
   avatar?: string;
 }
+
+export const registrationUser = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, email, password } = req.body;
+      const isEmailExist = await userModel.findOne({ email });
+
+      if (isEmailExist) {
+        return next(new ErrorHandler("Email already exists", 400));
+      }
+
+      const user: IRegisterBody = {
+        name,
+        email,
+        password,
+      };
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
