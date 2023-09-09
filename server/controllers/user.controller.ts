9109773330e77f470dd2ea55ec7b0ba32330abe5
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-09-09 19:36:06 +0600

    createActivationToken added

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from "express";
-import userModel, { IUser } from "../models/user.model";
+import userModel from "../models/user.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import { catchAsyncError } from "../middleware/catchAsyncErrors";
+import jwt, { Secret } from "jsonwebtoken";
 
 //register user
 interface IRegisterBody {
@@ -26,8 +27,26 @@ export const registrationUser = catchAsyncError(
         email,
         password,
       };
+
+      const activationToken = createActivationToken(user);
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
     }
   }
 );
+
+interface IActivationToken {
+  token: string;
+  activationCode: string;
+}
+
+export const createActivationToken = (user: any): IActivationToken => {
+  const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
+  const token = jwt.sign(
+    { user, activationCode },
+    process.env.ACTIVATION_SECRET as Secret,
+    { expiresIn: "5m" }
+  );
+
+  return { token, activationCode };
+};
