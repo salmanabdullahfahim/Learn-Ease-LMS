d4e48ced742e3e7ac1688162d8dbb2c54ab8b1d6
Salman Abdullah Fahim <77009176+salmanabdullahfahim@users.noreply.github.com>
2023-10-03 07:28:11 +0600

    Delete server directory for some issues
    I will make the application from scratch with another structure

--- server/server.ts
+++
@@ -1,9 +0,0 @@
-import { app } from "./app";
-import dbConnect from "./utils/db";
-require("dotenv").config();
-
-//listen server
-app.listen(process.env.PORT, () => {
-  console.log(`server listening on port ${process.env.PORT}`);
-  dbConnect();
-});
