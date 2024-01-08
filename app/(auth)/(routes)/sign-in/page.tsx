6257d480a74d0452d19b13952d616cb9f6813d5f
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-01-08 11:41:15 +0600

    created folder structure for the project

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,3 @@
+export default function page() {
+  return <div>This is an unprotected route</div>;
+}
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>This is a protected route</h2>
+    </div>
+  );
+}
