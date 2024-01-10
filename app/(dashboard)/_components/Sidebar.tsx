587549591f0c9c2bbb4f353f89496f84baa31982
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-01-10 11:15:30 +0600

    added a layout for dashboard and basic skeleton of sidebar

---
+++ app/(dashboard)/_components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <div className="h-full border-r flex  flex-col overflow-y-auto bg-white shadow-sm ">
+      Sidebar
+    </div>
+  );
+}
