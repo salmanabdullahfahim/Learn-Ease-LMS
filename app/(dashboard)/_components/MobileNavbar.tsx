dde9a44b96ed5ed58319787b58d738beac137889
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-01-11 15:36:00 +0600

    added a sidebar for mobile responsive

--- app/(dashboard)/_components/MobileNavbar.tsx
+++ app/(dashboard)/_components/MobileNavbar.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "./Sidebar";
 
 export default function MobileNavbar() {
-  return <Menu />;
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="bg-white p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
 }
