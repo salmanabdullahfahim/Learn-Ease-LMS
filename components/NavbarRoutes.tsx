046ed103dd59eca29352070b490595da6769094e
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-01-12 18:37:57 +0600

    make dynamic layout for navbar

--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -1,12 +1,35 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 export default function NavbarRoutes() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname.includes("/chapter");
   return (
     <div className="flex gap-x-3 ml-auto">
-      <UserButton />
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button variant="ghost" size="sm">
+            <LogOut className="h-4 w-4 mr-3" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button variant="ghost" size="sm">
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
